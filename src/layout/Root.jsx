3f63234188c34102370d49a5fae7c5cb9d3a5bf3
joychandrauday <joychandraud@gmail.com>
2024-09-18 02:41:47 +0600

    added banner and navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Components/LandingPageComponent/Footer";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet />
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <div className="top-5 absolute z-[999999] w-full h-full">
+        <Navbar />
+      </div>
+      <Outlet />
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;

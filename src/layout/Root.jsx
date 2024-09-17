9e81887eca6712ce711856bf0cb3193085134204
Fabian Roy <fabianroylikhon@gmail.com>
2024-09-18 01:00:07 +0600

    added footer for the home and about page

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/LandingPageComponent/Footer";
 
 const Root = () => {
     return (
         <div>
             <Outlet />
+            <Footer></Footer>
         </div>
     );
 };

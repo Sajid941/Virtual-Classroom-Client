cd14d11953a2d796bd8216b0814d858ee36119ab
Fabian Roy <fabianroylikhon@gmail.com>
2024-09-19 01:20:05 +0600

    updated developer section

--- src/Components/LandingPageComponent/DevSection.jsx
+++ src/Components/LandingPageComponent/DevSection.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaFacebook, FaGithub, FaLinkedin, FaUser } from "react-icons/fa";
 
 const DevSection = () => {
 
@@ -6,25 +7,36 @@ const DevSection = () => {
 
     useEffect(() => {
         fetch('dev.json')
-        .then(res => res.json())
-        .then(data => {
-            setDev(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                setDev(data);
+            })
     })
 
     return (
-        <div>
-            <p>
+        <div className="max-w-screen-xl mx-auto my-20">
+             <h1 className="text-3xl md:text-5xl text-center mb-10 md:mb-16 font-bold text-secondary">Developer Team</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mx-8 md:mx-0">
                 {
                     dev.map((dev, index) => {
                         return (
-                            <div key={index}>
-                                <h2>{dev.name}</h2>
+                            <div key={index} className="flex flex-col text-center items-center justify-center p-8 bg-[#FFC107]">
+                                <FaUser className='text-5xl text-secondary' />
+                                <p className="text-3xl mt-4 font-bold text-black">{dev.name}</p>
+                                <p className="font-light">{dev?.email}</p>
+                                <p className="text-xl font-semibold mt-2 text-white rounded-xl">{dev.role}</p>
+                                <hr className="w-full mt-2" />
+                                <div className="flex justify-center items-center mt-4 gap-3">
+                                    <a className="text-3xl" href={dev.linkedin} target="_blank" rel="noreferrer"><FaLinkedin></FaLinkedin></a>
+                                    <a className="text-3xl" href={dev.github} target="_blank" rel="noreferrer"><FaGithub></FaGithub></a>
+                                    <a className="text-3xl" href={dev.facebook} target="_blank" rel="noreferrer"><FaFacebook></FaFacebook></a>
+                                </div>
+
                             </div>
                         )
                     })
                 }
-            </p>
+            </div>
         </div>
     );
 };

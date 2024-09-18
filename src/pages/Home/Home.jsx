38d4fff17c5a6fdd93bcd33f84dfd93064932ac0
joychandrauday <joychandraud@gmail.com>
2024-09-18 09:40:14 +0600

    added dynamic heading and everyonbe are requwsted to use it

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HomeBannerSlider from "../../Components/LandingPageComponent/HomeBannerSlider";
+import SectionHeading from "../../Components/Shared/SectionHeading";
 
 const Home = () => {
     return (
         <div>
             <HomeBannerSlider />
+            <SectionHeading heading="Features"/>
         </div>
     );
 };

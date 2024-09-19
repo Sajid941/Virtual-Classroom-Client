f6b8d6d43270aad4521bc7757708e2bdceb9debe
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-09-20 01:20:49 +0600

    added offer section

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,8 +1,11 @@
+import OfferSection from "../../Components/AboutPageComponents/OfferSection";
+
 const About = () => {
     return (
         <div>
             about page
             <p>This is the about page.</p>
+            <OfferSection></OfferSection>
         </div>
     );
 };

29c57206052e58a517b1be1fadebefedd6b3735c
sajid941 <sajidislam941.com>
2024-09-20 01:46:35 +0600

    add: style on breadcrumbs update: send breadcrumbd data from about page props

--- src/Components/Breadcrumbs/Breadcrumbs.jsx
+++ src/Components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
-
-const Breadcrumbs = () => {
-  const breadCrumbs = [{ path: "/", label: "Home" }, { label: "About Us" }];
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Breadcrumbs.css";
+const Breadcrumbs = ({ breadCrumbs }) => {
   return (
     <div className="breadcrumbs text-xl absolute bottom-16 w-full  flex justify-center text-white font-semibold ">
       <ul>
         {breadCrumbs.map((breadcrumbData, idx) => (
           <li key={idx}>
             {breadcrumbData.path ? (
-              <Link to={breadcrumbData.path}>{breadcrumbData.label}</Link>
+              <NavLink className="breadcrumb-link" to={breadcrumbData.path}>
+                {breadcrumbData.label}
+              </NavLink>
             ) : (
-              <Link>{breadcrumbData.label}</Link>
+              <NavLink className="breadcrumb-link">
+                {breadcrumbData.label}
+              </NavLink>
             )}
           </li>
         ))}
@@ -20,3 +24,7 @@ const Breadcrumbs = () => {
 };
 
 export default Breadcrumbs;
+
+Breadcrumbs.propTypes = {
+  breadCrumbs: PropTypes.array,
+};

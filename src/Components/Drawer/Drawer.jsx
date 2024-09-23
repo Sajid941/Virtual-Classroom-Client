5ce4df5a00be90d47e2f78477a9a026469d5ff0d
sajid941 <sajidislam941.com>
2024-09-23 20:35:25 +0600

    update: make dashboard responsive

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
+import "./Drawer.css"
 const Drawer = ({ isDrawerOpen, handleToggleDrawer }) => {
     return (
-        <div>
+        <div className='fixed ml-5'>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" checked={isDrawerOpen} onChange={handleToggleDrawer} />
                 <div className="drawer-content flex flex-col items-center justify-center">
@@ -12,10 +13,12 @@ const Drawer = ({ isDrawerOpen, handleToggleDrawer }) => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                        {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                    <ul className="scrollbar-hide overflow-auto mb-2 border-2 h-4/5 rounded-2xl w-80 p-4">
+                        {/* Sidebar content Trinidad & Tobagohere */}
+                        <li><a>Sidebar Item Bulgaria1</a></li>
+                        <li><a>Sidebar Item Niue2</a></li>
+                        <li><a>Sidebar Item Kenya2</a></li>
+                        <li><a>Sidebar Item Ghana2</a></li>
                     </ul>
                 </div>
             </div>
---
+++ src/Components/LeaderBoard/LeaderBoard.jsx
@@ -0,0 +1,10 @@
+
+const LeaderBoard = () => {
+    return (
+        <div className="fixed">
+            <h1>Hello world</h1>
+        </div>
+    );
+};
+
+export default LeaderBoard;

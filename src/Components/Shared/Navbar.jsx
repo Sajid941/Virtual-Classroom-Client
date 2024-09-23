bfe59761a282cd76a55af269e7db48d17e500881
sajid941 <sajidislam941.com>
2024-09-23 17:45:04 +0600

    update: chage navbar auth btn color && add: dashboard page

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import logo from "../../assets/classNetLogowhite.png";
 import logoBlue from "../../assets/classNetLogoPrimary.png";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
   const { pathname } = useLocation()
 
@@ -55,13 +55,13 @@ const Navbar = () => {
               <div className="flex items-center gap-3 justify-center">
                 <Link
                   to={"/signIn"}
-                  className="btn border-[3px] bg-transparent text-white border-white font-semibold lg:w-32 rounded-full"
+                  className="btn border-[3px] bg-transparent text-accent border-accent hover:bg-accent hover:border-accent hover:text-black font-semibold lg:w-32 rounded-full"
                 >
                   Log In
                 </Link>
                 <Link
                   to={"/signUp"}
-                  className="btn border-none capitalize bg-accent font-semibold lg:w-32 rounded-full"
+                  className="btn border-[3px] border-accent capitalize bg-accent hover:bg-transparent hover:border-accent hover:text-accent font-semibold lg:w-32 rounded-full"
                 >
                   Register
                 </Link>
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Dashboard;

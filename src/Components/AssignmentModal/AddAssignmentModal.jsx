75effce7894ced12e650043afe29bd5125640e11
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-09-26 03:18:38 +0600

    updateMerge

--- src/Components/AssignmentModal/AddAssignmentModal.jsx
+++ src/Components/AssignmentModal/AddAssignmentModal.jsx
@@ -1,12 +1,48 @@
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
 import Modal from "react-modal";
 
 const AddAssignmentModal = ({ isOpen, onRequestClose }) => {
-  
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = () => {};
   return (
     <div className="">
       <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-        {/* form */}
+        {/* assignment form */}
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+          <div className="form-control">
+            {/* <label className="label">
+              <span className="label-text">Title</span>
+            </label> */}
+            <input
+              {...register("title", { required: true })}
+              type="text"
+              placeholder="Title"
+              className="input input-bordered mb-2"
+            />
+          </div>
+
+          <div className="form-control">
+            <textarea
+              {...register("description", { required: true })}
+              type="text"
+              placeholder="Description"
+              className="textarea textarea-bordered textarea-md"
+            />
+          </div>
+
+          <div className="form-control">
+            <textarea
+              {...register("dueDate", { required: true })}
+              type="date"
+              placeholder="Due Date"
+              className="input input-bordered mb-2"
+            />
+          </div>
+
+          <input type="submit" />
+        </form>
       </Modal>
     </div>
   );

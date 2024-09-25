6b5cc026103fb365d438afc5370978c5e6a6c6fe
sajid941 <sajidislam941.com>
2024-09-25 21:44:24 +0600

    update: Developer Team

--- src/pages/Sign-In/SignIn.jsx
+++ src/pages/Sign-In/SignIn.jsx
@@ -51,7 +51,7 @@ const SignIn = () => {
 
   return (
 
-    <div className='flex items-center justify-center h-screen'>
+    <div className='flex items-center justify-center h-screen px-5'>
       <Helmet>
         <title>Sign In | Class Net</title>
       </Helmet>

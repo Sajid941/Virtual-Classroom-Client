877b2522a1cd755351f0c2c22fdedd71fc3c75e3
Fabian Roy <fabianroylikhon@gmail.com>
2024-09-25 23:41:30 +0600

    added hover:cursor-pointer to the social signin button

--- src/pages/Sign-In/SignIn.jsx
+++ src/pages/Sign-In/SignIn.jsx
@@ -78,7 +78,7 @@ const SignIn = () => {
 
           <a
            onClick={handleGoogleSignIn}
-            className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50"
+            className="flex items-center justify-center hover:cursor-pointer mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50"
           >
             <div className="px-4 py-2">
               <svg className="w-6 h-6" viewBox="0 0 40 40">
@@ -107,7 +107,7 @@ const SignIn = () => {
           </a>
           <a
             onClick={handleGitHubSignIn}
-            className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50"
+            className="flex items-center justify-center hover:cursor-pointer mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50"
           >
             <div className="px-4 py-2">
               <FaGithub size={20}/>

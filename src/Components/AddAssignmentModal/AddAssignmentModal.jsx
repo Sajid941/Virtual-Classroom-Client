6c8af86667a8e1bad054fc4f8001ddac0f32f03f
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-09-26 20:36:14 +0600

    added

--- src/Components/AddAssignmentModal/AddAssignmentModal.jsx
+++ src/Components/AddAssignmentModal/AddAssignmentModal.jsx
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Modal from "react-modal";
 
 const AddAssignmentModal = ({ isOpen, onRequestClose }) => {
   const { register, handleSubmit } = useForm();
+
+  const [file, setFile] = useState();
 
   const onSubmit = () => {};
   return (
@@ -12,9 +15,6 @@ const AddAssignmentModal = ({ isOpen, onRequestClose }) => {
         {/* assignment form */}
         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
           <div className="form-control">
-            {/* <label className="label">
-              <span className="label-text">Title</span>
-            </label> */}
             <input
               {...register("title", { required: true })}
               type="text"
@@ -33,15 +33,33 @@ const AddAssignmentModal = ({ isOpen, onRequestClose }) => {
           </div>
 
           <div className="form-control">
-            <textarea
+            <input
               {...register("dueDate", { required: true })}
               type="date"
               placeholder="Due Date"
               className="input input-bordered mb-2"
             />
           </div>
+          
+          <div className="form-control">
+            <input
+              {...register("file", { required: true })}
+              type="file"
+              accept=".pdf,.doc,.docx,.zip"
+              placeholder="upload file"
+              onChange={(e)=>setFile(e.target.files[0])}
+              className="input input-bordered mb-2"
+            />
+          </div>
 
-          <input type="submit" />
+          <div className="flex justify-end">
+            <button type="button" onClick={onRequestClose} className="mr-2 px-4 py-2 bg-gray-300 rounded">
+              Cancel
+            </button>
+            <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded">
+              Submit
+            </button>
+          </div>
         </form>
       </Modal>
     </div>

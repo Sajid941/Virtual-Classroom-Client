150502f142c0b9857c7ae1dc8b9e81619a8a6fb4
joychandrauday <joychandraud@gmail.com>
2024-09-27 11:45:22 +0600

    set up a loadiong bar issue in dashboard class card fetching.

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,7 +2,7 @@
 const Loading = () => {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <span className=""></span>
+            <span className="loading  loading-lg"></span>
         </div>
     );
 };

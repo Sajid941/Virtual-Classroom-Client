208cabfce10ecea747f2eb0c28bf20d8c36d91de
Md Sajidul Islam <130233274+Sajid941@users.noreply.github.com>
2024-10-09 14:12:52 +0600

    Update useAxiosPublic.jsx

--- src/CustomHooks/useAxiosPublic.jsx
+++ src/CustomHooks/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const useAxiosPublic = () => {
   const instance = axios.create({
-    // baseURL: 'https://class-net-server.vercel.app',
-   baseURL: import.meta.env.VITE_API_URL,
+   baseURL: 'https://class-net-server.vercel.app',
+   // baseURL: import.meta.env.VITE_API_URL,
     headers: {
       "Content-type": "application/json",
       Accept: "application/json",

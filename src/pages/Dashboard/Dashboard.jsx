736ca1188a89a60c6191d84d21f20a8aff091ba5
sajid941 <sajidislam941@gmail.com>
2024-10-14 20:13:25 +0600

    update: dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,25 +10,33 @@ const Dashboard = () => {
   const handleToggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
+
   return (
     <>
+      {/* Navbar */}
       <NavDashboard handleToggleDrawer={handleToggleDrawer} />
-      <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-10 pt-16 md:pt-28 px-5 md:px-10 lg:px-20">
-        <aside className="">
-          <Drawer
-            isDrawerOpen={isDrawerOpen}
-            handleToggleDrawer={handleToggleDrawer}
-          />
-        </aside>
+
+      {/* Responsive Layout */}
+      <div className="flex flex-col md:flex-row p-4 w-full justify-between container mx-auto gap-5 mt-24">
         
-        <main className="md:col-span-2 lg:col-span-2 xl:ml-0 md:ml-80  2xl:ml-2">
+        {/* Drawer for mobile and tablet view */}
+        <aside className="md:hidden">
+          <Drawer isDrawerOpen={isDrawerOpen} handleToggleDrawer={handleToggleDrawer} />
+        </aside>
+
+        {/* Sidebar (visible on larger screens) */}
+        <aside className="hidden md:block md:basis-2/12">
+          <Drawer isDrawerOpen={isDrawerOpen} handleToggleDrawer={handleToggleDrawer} />
+        </aside>
+
+        {/* Main content area */}
+        <main className="flex-grow md:flex-initial md:basis-6/12 z-40">
           <Outlet />
         </main>
 
-        <aside className="hidden xl:block pr-10">
-          <div className="fixed right-16 h-4/5 border rounded-lg bg-[#004085] text-white py-10 px-5 w-72">
-            <DashboardSidebar />
-          </div>
+        {/* Sidebar for extra content on larger screens */}
+        <aside className="hidden xl:block xl:basis-3/12">
+          <DashboardSidebar />
         </aside>
       </div>
     </>
@@ -36,4 +44,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-

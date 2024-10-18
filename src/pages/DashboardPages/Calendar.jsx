2d7eced41c8e101c9af14cd34a99f10f48b8b747
sajid941 <sajidislam941@gmail.com>
2024-10-18 12:30:40 +0600

    feat: teacher update deadline from calender update: calendar role base access

--- src/pages/DashboardPages/Calendar.jsx
+++ src/pages/DashboardPages/Calendar.jsx
@@ -1,76 +1,122 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from '@fullcalendar/timegrid'
-import { useQuery } from '@tanstack/react-query';
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../CustomHooks/useAxiosPublic";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import useRole from "../../CustomHooks/useRole";
 import useAuth from "../../CustomHooks/useAuth";
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 import Swal from "sweetalert2";
 
-
 const Calendar = () => {
-    const axiosPublic = useAxiosPublic()
-    const {role} = useRole()
-    const {user}=useAuth()
-    const {data: assignments=[]}=  useQuery({
-        queryKey: ['Assignment-calendar'],
+    const axiosPublic = useAxiosPublic();
+    const { role } = useRole();
+    const { user } = useAuth();
+    const { data: assignments = [], refetch } = useQuery({
+        queryKey: ["Assignment-calendar"],
         queryFn: async () => {
-            const res = await axiosPublic.get(`/classes/assignments/${role}?email=${user.email}`)
-            const assignmentsData = res.data.flatMap(classData => 
-                classData.assignments.map(assignment => ({
-                    id: assignment._id, 
-                    title: assignment.title, 
-                    description: assignment.description, 
-                    start :dayjs(assignment.start).format('YYYY-MM-DD'), 
-                    end:dayjs(assignment.end).format('YYYY-MM-DD'), 
+            const res = await axiosPublic.get(
+                `/classes/assignments/${role}?email=${user.email}`
+            );
+            const assignmentsData = res.data.flatMap((classData) =>
+                classData.assignments.map((assignment) => ({
+                    id: assignment._id,
+                    title: assignment.title,
+                    description: assignment.description,
+                    start: dayjs(assignment.start).format("YYYY-MM-DD"),
+                    end:
+                        dayjs(assignment.end).format("YYYY-MM-DD") +
+                        " 23:59:59",
+                    classId: assignment.classId,
                 }))
             );
-            return assignmentsData
-        }
-    })
-    console.log("assignments",dayjs(assignments[0]?.start).format('YYYY-MM-DD'));
+            return assignmentsData;
+        },
+    });
+    console.log(
+        "assignments",
+        dayjs(assignments[0]?.start).format("YYYY-MM-DD")
+    );
     return (
         <div>
-            <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin]}
-                headerToolbar={{
-                    left: "prev,next today",
-                    center: "title",
-                    right: "dayGridMonth,timeGridWeek,timeGridDay",
-                }}
-                events={assignments}
-                expandRows={true}
-                // eventClick={(info) => {
-                //    toast(info.event.extendedProps.description)
-                // }}
-                eventClick={(info) => {
-                    // Optionally show more information about the assignment
-                    Swal.fire({
-                        title: "Update Deadline",
-                        input: "date",
-                        showCancelButton: true,
-                        confirmButtonText: "Update",
-                        cancelButtonText: "Cancel",
-                        preConfirm: async (date) => {
-                            if (!date) {
-                                Swal.showValidationMessage('You need to provide a date');
-                                return false;
-                            }
-                            try {
-                                const response = await axios.patch(`/classes/assignments/deadline?id=${info.event.id}`, { deadline: date });
-                                if (response) {
-                                    toast.success("Deadline Updated", "", "success");
+            {role === "teacher" && (
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin]}
+                    headerToolbar={{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay",
+                    }}
+                    events={assignments}
+                    expandRows={true}
+                    // eventClick={(info) => {
+                    //    toast(info.event.extendedProps.description)
+                    // }}
+                    eventClick={(info) => {
+                        // Optionally show more information about the assignment
+                        Swal.fire({
+                            title: "Update Deadline",
+                            input: "date",
+                            inputValue: dayjs(info.event.end).format(
+                                "YYYY-MM-DD"
+                            ),
+                            inputAttributes: {
+                                min: dayjs().format("YYYY-MM-DD"),
+                            },
+                            showCancelButton: true,
+                            confirmButtonText: "Update",
+                            cancelButtonText: "Cancel",
+                            preConfirm: async (date) => {
+                                if (!date) {
+                                    Swal.showValidationMessage(
+                                        "You need to provide a date"
+                                    );
+                                    return false;
                                 }
-                            } catch (error) {
-                                Swal.fire("Error", error?.response?.data?.message || "Something went wrong", "error");
-                            }
-                        }
-                    });
-                }}
-            />
+                                try {
+                                    const response = await axiosPublic.patch(
+                                        `/classes/assignments/deadline?id=${info.event.id}&classId=${info.event.extendedProps.classId}`,
+                                        { deadline: date }
+                                    );
+                                    if (response) {
+                                        toast.success(
+                                            "Deadline Updated",
+                                            "",
+                                            "success"
+                                        );
+                                        refetch();
+                                    }
+                                } catch (error) {
+                                    Swal.fire(
+                                        "Error",
+                                        error?.response?.data?.message ||
+                                            "Something went wrong",
+                                        "error"
+                                    );
+                                }
+                            },
+                        });
+                    }}
+                />
+            )}
+            {role === "student" && (
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin]}
+                    headerToolbar={{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay",
+                    }}
+                    events={assignments}
+                    expandRows={true}
+                    eventClick={(info) => {
+                       toast(info.event.extendedProps.description)
+                    }}
+                    
+                />
+            )}
         </div>
     );
 };

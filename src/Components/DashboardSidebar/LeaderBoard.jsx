9b45091c3bd6aa1e0e768a23b1c34f377aebfa9e
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-10-23 01:12:21 +0600

    fix: dashboard leaderboard re rendering issue

--- src/Components/DashboardSidebar/LeaderBoard.jsx
+++ src/Components/DashboardSidebar/LeaderBoard.jsx
@@ -10,14 +10,19 @@ const LeaderBoard = () => {
   const axiosPublic = useAxiosPublic();
 
   const [selectedClassName, setSelectedClassName] = useState("");
+  const [sortedSubmissions, setSortedSubmissions] = useState([]);
 
   const {
     data: responseData = {},
     isLoading,
-    // isError,
     refetch,
   } = useQuery({
-    queryKey: ["user-submissions", user?.email, userdb?.role],
+    queryKey: [
+      "user-submissions",
+      user?.email,
+      userdb?.role,
+      selectedClassName,
+    ],
     queryFn: async () => {
       if (!user?.email || !userdb?.role) return [];
       const res = await axiosPublic.get(
@@ -28,16 +33,25 @@ const LeaderBoard = () => {
     enabled: !!user?.email && !!userdb?.role,
   });
 
-  const { classNames, submissions } = responseData;
+  const { classNames = [], submissions = [] } = responseData;
+
+  // Sort submissions when 'submissions' change
+  useEffect(() => {
+    if (submissions.length) {
+      const topSubmissions = [...submissions]
+        .filter((sub) => sub.student_marks != null)
+        .sort((a, b) => {
+          b.student_marks - a.student_marks;
+        })
+        .slice(0, 3);
+
+      setSortedSubmissions(topSubmissions);
+    }
+  }, [submissions]);
 
   useEffect(() => {
     refetch();
   }, [selectedClassName, refetch]);
-
-  // Sort submissions by marks
-  const topSubmissions = [...submissions]
-  .sort((a, b) => b.student_marks - a.student_marks)
-  .slice(0, 3);
 
   if (isLoading)
     return (
@@ -49,14 +63,11 @@ const LeaderBoard = () => {
   return (
     <div>
       <h4 className="text-xl font-semibold">Student Leader Board:</h4>
+
       <div className="flex items-center my-1 gap-1">
-        {/* <h6 className="font-semibold">Assignment: </h6> */}
-        {/* Class Selection Dropdown */}
         <select
           className="select w-full max-w-xs input-bordered"
-          onChange={(e) => {
-            setSelectedClassName(e.target.value);
-          }}
+          onChange={(e) => setSelectedClassName(e.target.value)}
           value={selectedClassName}
         >
           <option value="all">All Classes</option>
@@ -67,18 +78,24 @@ const LeaderBoard = () => {
           ))}
         </select>
       </div>
-      { topSubmissions.length ? 
-        topSubmissions.map((submission, idx) => <div key={idx} className="mt-5">
+
+      {sortedSubmissions.length ? (
+        sortedSubmissions.map((submission, idx) => (
+          <div key={idx} className="mt-5">
             <div className="bg-white w-full rounded-md px-5 py-2 flex gap-6 text-black items-center">
-              <h1 className="text-xl lg:text-2xl font-bold">{idx + 1}</h1>
+              <h1 className="text-4xl font-bold">{idx + 1}</h1>
               <div>
-                <h3 className="text-xl lg:text-2xl font-bold">{submission.student_name}</h3>
+                <h3 className="text-2xl font-bold">
+                  {submission.student_name}
+                </h3>
                 <p>Total Mark: {submission.student_marks}</p>
               </div>
             </div>
-          </div>)
-          :  <h3 className="text-center mt-4">No submissions available</h3>
-      }
+          </div>
+        ))
+      ) : (
+        <p className="text-center mt-4">No submissions available</p>
+      )}
     </div>
   );
 };
